f0a5c6bb47a89a31b1edb08a66bab7eb59165f62
MaKraus <damad@gmx.net>
2012-03-24 11:11:54 +0100

    Restore basic functionality

--- Image-Tagger-WinForms/ImageTagger.cs
+++ Image-Tagger-WinForms/ImageTagger.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ImageTaggerWinForms
 {
 	public class ImageTagger : Form
 	{
+		PictureBox mainImage = new PictureBox();
+		
 		public static void Main ()
 		{
 			Application.Run (new ImageTagger ());
@@ -28,31 +31,55 @@ namespace ImageTaggerWinForms
 			leftRightSplitter.IsSplitterFixed = true;
 						
 			// Panel 1
-			leftRightSplitter.Panel1MinSize = 310;
+			leftRightSplitter.Panel1MinSize = 210;
 			leftRightSplitter.Panel1.BackColor = Color.AntiqueWhite;
 			FlowLayoutPanel imageList = new FlowLayoutPanel();
-			imageList.FlowDirection = FlowDirection.TopDown;
+			imageList.FlowDirection = FlowDirection.LeftToRight;
+			
 			
 			imageList.SuspendLayout();
 			imageList.Dock = DockStyle.Fill;
 			imageList.AutoScroll = true;
-			PictureBox image1 = new PictureBox();
-			image1.Width = 300;
-			image1.Height = 300;
-				
-			image1.Load("/storage/bilder/Digicam Nachbearbeitet/NachDemRegen.JPG");
-			imageList.Controls.Add(image1);
 			
+			DirectoryInfo imageDir = new DirectoryInfo("/storage/bilder/Digicam Nachbearbeitet/");
+			var files = imageDir.GetFiles("*", SearchOption.AllDirectories);
+			
+			foreach(var file in files)
+			{
+				if(file.Extension.ToLower() != ".jpg")
+				{
+					continue;
+				}
+				
+				PictureBox image1 = new PictureBox();
+				image1.Width = 200;
+				image1.Height = 150;
+				image1.SizeMode = PictureBoxSizeMode.Zoom;
+				image1.Click += HandleImage1Click;
+				
+				image1.Load(file.FullName);
+				imageList.Controls.Add(image1);
+			}
 			leftRightSplitter.Panel1.Controls.Add(imageList);
 			imageList.ResumeLayout();
 			
 			// Panel 2
-			TextBox hello = new TextBox();
-			hello.Text = "Hello World!";
-			leftRightSplitter.Panel2.Controls.Add(hello);
+			mainImage.Dock = DockStyle.Fill;
+			mainImage.SizeMode = PictureBoxSizeMode.Zoom;
+						
+			leftRightSplitter.Panel2.Controls.Add(mainImage);
 						
 			leftRightSplitter.ResumeLayout(false);
 			this.ResumeLayout(false);
+		}
+
+		void HandleImage1Click (object sender, EventArgs e)
+		{
+			if(sender is PictureBox)
+			{
+				PictureBox listImage = (PictureBox) sender;	
+				mainImage.Load(listImage.ImageLocation);				
+			}
 		}
 	}
 }
